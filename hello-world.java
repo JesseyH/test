5ce91fce7480482aae57a513d72589be0941cd5c
Jessey H <jessey.harry.23@gmail.com>
2014-12-08 22:21:51 -0500

    Added JFrame
    JFrame does nothing :'(
    need to add some cool stuff

--- hello-world.java
+++ hello-world.java
@@ -1,5 +1,10 @@
 public class hello-world{
   public static void hello-world(String[] args) {
+    JFrame frame = new JFram("Hello World");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+    frame.pack();
+    frame.setVisible(true);
     System.out.println("Hello World");
   }
 }  
